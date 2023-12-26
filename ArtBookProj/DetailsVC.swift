1af7d4eb1612b7dc498b38fa6489d06760cb3700
atilla.citirik <atillactrk@gmail.com>
2023-12-26 18:18:16 +0300

    UI tasarlama

---
+++ ArtBookProj/DetailsVC.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsVC.swift
+//  ArtBookProj
+//
+//  Created by Atilla Çıtırık on 26.12.2023.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtArtist: UITextField!
+    @IBOutlet weak var txtYear: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnSaveClicked(_ sender: Any) {
+    }
+    
+
+}
